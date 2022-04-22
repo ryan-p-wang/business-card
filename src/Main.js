5f9f57327291c37ab05dcd39be9cb0ca1f369d91
Ryan Wang <ryan.wang.mi@gmail.com>
2022-04-22 14:46:45 -0400

    second commit

--- src/Main.js
+++ src/Main.js
@@ -5,14 +5,19 @@ export default function Main() {
             <h3 className="main-subtitle">Incoming First-Year Student @ Yale</h3>
             
             <div className="contacts">
-                <button type="button">
-                    <img src={require("./email.png")} width="20px" />
-                    <h4>Email</h4>
-                </button>
-                <button type="button">
-                    <img src={require("./linkedin.png")} width="20px" />
-                    <h4>LinkedIn</h4>
-                </button>
+                <a href="mailto: ryan.wang.mi@gmail.com" target="_blank">
+                    <button type="button">
+                        <img src={require("./email.png")} width="20px" />
+                        <h4>Email</h4>
+                    </button>
+                </a>
+
+                <a href="https://www.linkedin.com/in/ryan-wang-a818211b1/" target="_blank">
+                    <button type="button" className="linkedin">
+                        <img src={require("./linkedin.png")} width="20px" />
+                        <h4>LinkedIn</h4>
+                    </button>
+                </a>
             </div>
 
             <h2>About</h2>
